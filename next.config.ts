784b5c4a90323ecf0f29a29bccfefd406c8c7b61
reybaeb <reybaeb@users.noreply.github.com>
2025-12-09 14:36:10 +0700

    fix: disable PWA and revert build script to resolve vercel worker error

--- next.config.ts
+++ next.config.ts
@@ -29,13 +29,15 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default withPWA({
-  dest: "public",
-  cacheOnFrontEndNav: true,
-  aggressiveFrontEndNavCaching: true,
-  reloadOnOnline: true,
-  disable: process.env.NODE_ENV === "development",
-  workboxOptions: {
-    disableDevLogs: true,
-  },
-})(nextConfig);
+// export default withPWA({
+//   dest: "public",
+//   cacheOnFrontEndNav: true,
+//   aggressiveFrontEndNavCaching: true,
+//   reloadOnOnline: true,
+//   disable: process.env.NODE_ENV === "development",
+//   workboxOptions: {
+//     disableDevLogs: true,
+//   },
+// })(nextConfig);
+
+export default nextConfig;
